debe5a64ac0cab55301244f6c544bb7998f3fdf6
alessiavozzo <a.vozzo95@gmail.com>
2024-03-21 18:10:12 +0100

    add footer component

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -5,7 +5,47 @@ export default {
 </script>
 
 <template>
-    <h1>footer</h1>
+    <!-- /#site_footer -->
+    <footer id="site_footer">
+
+        <div class="container">
+            <!-- /.copyright -->
+            <div class="copyright">
+                <h4>Boolando srl</h4>
+                <a href="#">Informazioni legali </a>
+                <a href="#">Informativa sulla privacy </a>
+                <a href="#">Diritti di recesso</a>
+            </div>
+
+            <!-- /.social -->
+            <div class="social">
+
+                <h4>Trovaci anche su:</h4>
+
+                <div class="social-icons">
+                    <a href="#">
+                        <i class="fa-brands fa-square-twitter"></i>
+                    </a>
+                    <a href="#">
+                        <i class="fa-brands fa-square-facebook"></i>
+                    </a>
+                    <a href="#">
+                        <i class="fa-brands fa-square-instagram"></i>
+                    </a>
+                    <a href="#">
+                        <i class="fa-brands fa-square-pinterest"></i>
+                    </a>
+                    <a href="#">
+                        <i class="fa-brands fa-square-youtube"></i>
+                    </a>
+                </div>
+
+            </div>
+        </div>
+
+
+
+    </footer>
 </template>
 
 <style></style>
