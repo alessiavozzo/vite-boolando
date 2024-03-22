cea88945241e5a10951abeff36b2e001079375d6
alessiavozzo <a.vozzo95@gmail.com>
2024-03-22 11:51:06 +0100

    add productCard component

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,12 +1,20 @@
 <script>
+import ProductCard from './ProductCard.vue';
 export default {
     name: "AppMain",
+    components: {
+        ProductCard
+    },
+    props: {
+        card: Object
+    },
 
     data() {
         return {
             hoverEffect: false,
             cards: [
                 {
+                    id: 1,
                     image: "/public/images/1.webp",
                     hoverImage: "/public/images/1b.webp",
                     brand: "Levi's",
@@ -17,6 +25,7 @@ export default {
                     newPrice: 14.99
                 },
                 {
+                    id: 2,
                     image: "/public/images/2.webp",
                     hoverImage: "/public/images/2b.webp",
                     brand: "Guess",
@@ -27,6 +36,7 @@ export default {
                     newPrice: 20.99
                 },
                 {
+                    id: 3,
                     image: "/public/images/3.webp",
                     hoverImage: "/public/images/3b.webp",
                     brand: "Come Zucchero Filato",
@@ -37,6 +47,7 @@ export default {
                     newPrice: 129.99
                 },
                 {
+                    id: 4,
                     image: "/public/images/4.webp",
                     hoverImage: "/public/images/4b.webp",
                     brand: "Levi's",
@@ -47,6 +58,7 @@ export default {
                     newPrice: 14.99
                 },
                 {
+                    id: 5,
                     image: "/public/images/5.webp",
                     hoverImage: "/public/images/5b.webp",
                     brand: "Maya Deluxe",
@@ -57,6 +69,7 @@ export default {
                     newPrice: 99.99
                 },
                 {
+                    id: 6,
                     image: "/public/images/6.webp",
                     hoverImage: "/public/images/6b.webp",
                     brand: "Esprit",
@@ -79,34 +92,9 @@ export default {
         <div class="container main">
 
             <div class="row">
-
-                <!-- Cards -->
-                <div class="card" v-for="card in cards" @mouseover="card.hoverEffect = true"
-                    @mouseleave="card.hoverEffect = false">
-                    <div class="item">
-                        <img :src="card.hoverEffect === true ? card.hoverImage : card.image" :alt="card.name">
-                    </div>
-
-                    <div class="info">
-                        <div class="discount" v-if="card.discount !== null">{{ card.discount }}</div>
-                        <div class="eco-friendly" v-if="card.eco">Sostenibilità</div>
-                    </div>
-
-                    <div class="text">
-                        <span class="brand">{{ card.brand }}</span>
-                        <h5>{{ card.name }}</h5>
-                        <span class="new-price">{{ card.newPrice }}&euro;</span>
-                        <span v-if="card.oldPrice !== null" class="old-price">{{ card.oldPrice }}&euro;</span>
-                    </div>
-
-                    <div class="heart">&hearts;</div>
-
-                </div>
-
+                <ProductCard :card="card" :key="card.id" v-for="card in cards" @mouseover="card.hoverEffect = true"
+                    @mouseleave="card.hoverEffect = false" />
             </div>
-
-
-
 
         </div>
 
---
+++ src/components/ProductCard.vue
@@ -0,0 +1,32 @@
+<script>
+export default {
+    name: "ProductCard",
+    props: ["card"]
+}
+</script>
+
+<template>
+    <!-- Cards -->
+    <div class="card">
+        <div class="item">
+            <img :src="card.hoverEffect === true ? card.hoverImage : card.image" :alt="card.name">
+        </div>
+
+        <div class="info">
+            <div class="discount" v-if="card.discount !== null">{{ card.discount }}</div>
+            <div class="eco-friendly" v-if="card.eco">Sostenibilità</div>
+        </div>
+
+        <div class="text">
+            <span class="brand">{{ card.brand }}</span>
+            <h5>{{ card.name }}</h5>
+            <span class="new-price">{{ card.newPrice }}&euro;</span>
+            <span v-if="card.oldPrice !== null" class="old-price">{{ card.oldPrice }}&euro;</span>
+        </div>
+
+        <div class="heart">&hearts;</div>
+
+    </div>
+</template>
+
+<style></style>
